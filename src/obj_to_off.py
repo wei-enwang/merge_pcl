657b4f696f9445cf26530fce1586fefd2c7fa1fc
wei-enwang <warrenwang6303@gmail.com>
2021-11-29 10:52:31 -0500

    obj to off working

--- src/obj_to_off.py
+++ src/obj_to_off.py
@@ -1,16 +1,18 @@
 import trimesh
+import os
 import os.path
 import glob
 
-trimesh.util.attach_to_log()
 
-output_dir = "../occupancy_network/external/mesh-fusion/output/"
-obj_list = sorted(glob.glob("./data/obj/ShapeNetCore.v2/*/*/model/model_normalized.obj"))
+# trimesh.util.attach_to_log()
+
+output_dir = "../occupancy_networks/external/mesh-fusion/output/"
+obj_list = sorted(glob.glob("./data/obj/ShapeNetCore.v2/*/*/models/model_normalized.obj"))
 print(len(obj_list))
 obj = obj_list[0]
 # for obj in obj_list:
 
-fileName = os.path.split(os.path.dirname(obj))[-2]
+fileName = os.path.normpath(obj).split(os.sep)[-3]
 mesh = trimesh.load(obj, force='mesh')
     # save as .off, retain file name
 mesh.export(output_dir+fileName+".off")
