5d0592c2e319685766d73d735239755db837c804
wei-enwang <warrenwang6303@gmail.com>
2021-11-29 10:55:59 -0500

    process all models

--- src/obj_to_off.py
+++ src/obj_to_off.py
@@ -3,16 +3,14 @@ import os
 import os.path
 import glob
 
-
-# trimesh.util.attach_to_log()
+trimesh.util.attach_to_log()
 
 output_dir = "../occupancy_networks/external/mesh-fusion/output/"
 obj_list = sorted(glob.glob("./data/obj/ShapeNetCore.v2/*/*/models/model_normalized.obj"))
-print(len(obj_list))
-obj = obj_list[0]
-# for obj in obj_list:
+# print(len(obj_list))
 
-fileName = os.path.normpath(obj).split(os.sep)[-3]
-mesh = trimesh.load(obj, force='mesh')
+for obj in obj_list:
+    fileName = os.path.normpath(obj).split(os.sep)[-3]
+    mesh = trimesh.load(obj, force='mesh')
     # save as .off, retain file name
-mesh.export(output_dir+fileName+".off")
+    mesh.export(output_dir+fileName+".off")
