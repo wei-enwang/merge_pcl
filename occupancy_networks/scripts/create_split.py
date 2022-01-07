f459c735a6921b7f0aaba819f21c68c31787b61e
wei-enwang <warrenwang6303@gmail.com>
2022-01-07 13:50:01 +0800

    remove terminal arguments, add recursive

--- occupancy_networks/scripts/create_split.py
+++ occupancy_networks/scripts/create_split.py
@@ -1,70 +1,72 @@
 import argparse
 import os
+import glob
 import random
 
-parser = argparse.ArgumentParser(
-    description='Split data into train, test and validation sets.')
-parser.add_argument('in_folder', type=str,
-                    help='Input folder where data is stored.')
+# parser = argparse.ArgumentParser(
+#     description='Split data into train, test and validation sets.')
+# parser.add_argument('in_folder', type=str,
+#                     help='Input folder where data is stored.')
 
-parser_nval = parser.add_mutually_exclusive_group(required=True)
-parser_nval.add_argument('--n_val', type=int,
-                         help='Size of validation set.')
-parser_nval.add_argument('--r_val', type=float,
-                         help='Relative size of validation set.')
+# parser_nval = parser.add_mutually_exclusive_group(required=True)
+# parser_nval.add_argument('--n_val', type=int,
+#                          help='Size of validation set.')
+# parser_nval.add_argument('--r_val', type=float,
+#                          help='Relative size of validation set.')
 
-parser_ntest = parser.add_mutually_exclusive_group(required=True)
-parser_ntest.add_argument('--n_test', type=int,
-                          help='Size of test set.')
-parser_ntest.add_argument('--r_test', type=float,
-                          help='Relative size of test set.')
+# parser_ntest = parser.add_mutually_exclusive_group(required=True)
+# parser_ntest.add_argument('--n_test', type=int,
+#                           help='Size of test set.')
+# parser_ntest.add_argument('--r_test', type=float,
+#                           help='Relative size of test set.')
 
-parser.add_argument('--shuffle', action='store_true')
-parser.add_argument('--seed', type=int, default=4)
+# parser.add_argument('--shuffle', action='store_true')
+# parser.add_argument('--seed', type=int, default=4)
 
-args = parser.parse_args()
+# args = parser.parse_args()
 
-if args.seed is not None:
-    random.seed(args.seed)
+"""make sure all files are in the SAME format under the directory"""
 
-all_samples = [name for name in os.listdir(args.in_folder)
-               if os.path.isdir(os.path.join(args.in_folder, name))]
+random.seed(32)
+r_val = 0.3
+r_test = 0.1
 
-if args.shuffle:
-    random.shuffle(all_samples)
+in_folder = "../data/pcl/"
 
-# Number of examples
-n_total = len(all_samples)
+for p in os.listdir(in_folder):
+    work_dir = os.path.join(in_folder, p)
+    if os.path.isdir(work_dir):
+        
+        samples = []
+        for type in ["*.off", "*.npz", "*.obj"]:
+            samples.extend(glob.glob(os.path.join(work_dir, "*.off")))
+        random.shuffle(samples)
 
-if args.n_val is not None:
-    n_val = args.n_val
-else:
-    n_val = int(args.r_val * n_total)
+        # Number of examples
+        n_total = len(samples)
 
-if args.n_test is not None:
-    n_test = args.n_test
-else:
-    n_test = int(args.r_test * n_total)
+        n_val = int(r_val * n_total)
+        n_test = int(r_test * n_total)
 
-if n_total < n_val + n_test:
-    print('Error: too few training samples.')
-    exit()
+        if n_total < n_val + n_test:
+            print('Error: too few training samples.')
+            exit()
 
-n_train = n_total - n_val - n_test
+        n_train = n_total - n_val - n_test
 
-assert(n_train >= 0)
+        assert(n_train >= 0)
 
-# Select elements
-train_set = all_samples[:n_train]
-val_set = all_samples[n_train:n_train+n_val]
-test_set = all_samples[n_train+n_val:]
+        # Select elements
+        train_set = samples[:n_train]
+        val_set = samples[n_train:n_train+n_val]
+        test_set = samples[n_train+n_val:]
 
-# Write to file
-with open(os.path.join(args.in_folder, 'train.lst'), 'w') as f:
-    f.write('\n'.join(train_set))
+        # Write to file
+        with open(os.path.join(work_dir, 'train.lst'), 'w') as f:
+            f.write('\n'.join(train_set))
 
-with open(os.path.join(args.in_folder, 'val.lst'), 'w') as f:
-    f.write('\n'.join(val_set))
+        with open(os.path.join(work_dir, 'val.lst'), 'w') as f:
+            f.write('\n'.join(val_set))
 
-with open(os.path.join(args.in_folder, 'test.lst'), 'w') as f:
-    f.write('\n'.join(test_set))
+        with open(os.path.join(work_dir, 'test.lst'), 'w') as f:
+            f.write('\n'.join(test_set))
